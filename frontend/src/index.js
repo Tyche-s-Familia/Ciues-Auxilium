396893ece844f8a9113c5b5a81484607e39e1931
Nevsimao <coday.ink@gmail.com>
2021-04-23 15:38:53 -0400

    finished kinda

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+export {default as Navbar} from './components/Navbar';
 
 ReactDOM.render(
   <React.StrictMode>
