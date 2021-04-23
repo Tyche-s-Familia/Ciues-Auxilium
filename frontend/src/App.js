30cdb6ca0a76cc6dfee6994309c076c61aeff2c0
Nevsimao <coday.ink@gmail.com>
2021-04-23 17:10:56 -0400

    clean ups

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,26 @@
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-// import {Navbar} from './components'
-import GlobalStyles from './GlobalStyles'
-import Navbar from './components/Navbar/Navbar';
+
+import {
+  GlobalStyles,
+  Wrapper,
+  TopNav,
+  Search,
+  Nav,
+  Main,
+  Foot
+} from './GlobalStyles';
 
 export default function App() {
   return (
     <Router>
       <GlobalStyles />
-      <Navbar />
+        <Wrapper>
+          <TopNav />
+          <Search />
+          <Nav />
+          <Main />
+          <Foot />
+        </Wrapper>
     </Router>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-export {default as Navbar} from './components/Navbar';
 
 ReactDOM.render(
   <React.StrictMode>
