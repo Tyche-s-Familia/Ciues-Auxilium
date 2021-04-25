77d5440283b71a34f17f589a346f3d8515623626
Nevsimao <coday.ink@gmail.com>
2021-04-25 19:12:01 -0400

    updated the Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Feed from './feedView/Feed'
 import Users from './userView/Users'
 import Details from './detailView/Details'
 
+import {
+  GlobalStyles
+} from './GlobalStyles'
+
 export default function App() {
 
   return (
     <Router>
-      <Route exact path='/' render={() => <Feed />} />
-      <Route exact path='/account' component={Users} />
-      <Route exact path='/details' component={Details} />
+      <GlobalStyles />
+      <Switch>
+        <Route exact path='/' render={Feed} />
+        <Route exact path='/account' component={Users} />
+        <Route exact path='/details' component={Details} />
+      </Switch>
     </Router>
   )
 }
