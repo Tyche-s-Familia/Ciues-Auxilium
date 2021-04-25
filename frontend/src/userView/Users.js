d8bdfa36fcc222ec626e8607a0bb36cc4b9028f5
Nevsimao <coday.ink@gmail.com>
2021-04-25 13:30:48 -0400

    started the user page

--- frontend/src/userView/Users.js
+++ frontend/src/userView/Users.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export const Users = () => {
-    return 'Hello User'
+    return (
+        <>
+            <h1>Hello User</h1>
+            <p>Dabadabadoba</p>
+        </>
+    )
 };
