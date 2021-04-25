cb145a6f0c923a907334470176c8175b3e0337ae
Nevsimao <coday.ink@gmail.com>
2021-04-25 14:54:07 -0400

    resized the rows and add coms related to Author

--- frontend/src/userView/UsersStyles.js
+++ frontend/src/userView/UsersStyles.js
@@ -1,22 +1,30 @@
-import styled, {createGlobalStyle} from "styled-components";
+import styled, {createGlobalStyle} from "styled-components"
 
 export const Wrapper = styled.section`
   width: 100vw;
   height: 100vh;
   display: grid;
   text-align: center;
-  grid-template-rows: 6vh 6vh 6vh 76vh 6vh;
+  grid-template-rows: 6vh 40vh 28vh 20vh 6vh;
   grid-template-areas: 'TopNav TopNav TopNav'
   'Author Author Author'
   'Project Project Project'
   'Supported Supported Supported'
-  'Foot Foot Foot';
+  'Foot Foot Foot'
 `;
 
 export const Author = styled.div`
     grid-area: Author;
     background: red;
+    grid-template-areas: 'AuthorName AuthorImg AuthorImg'
+    'AuthorBio AuthorBio AuthorDetail'
 `;
+
+export const AuthorName = styled.div``;
+export const AuthorBio = styled.div``;
+export const AuthorImg = styled.div``;
+export const AuthorDetail = styled.div``;
+
 export const Project = styled.div`
     grid-area: Project;
     background: pink;
