16a519b846cb9e93cb202bd2113df9bda82e49bb
Nevsimao <coday.ink@gmail.com>
2021-04-26 00:22:34 -0400

    moved to a separate comp

--- frontend/src/userView/Users.js
+++ frontend/src/userView/Users.js
@@ -1,63 +1,27 @@
-import React from 'react';
+import React from 'react'
+
+import TopNavBar from '../feedView/TopNavBar'
+import Footer from '../feedView/Footer'
+import AuthorView from './AuthorView'
+import ProjectView from './ProjectView'
+import SupportedView from './SupportedView'
 
 import {
-    TopNav,
-    Logo,
-    Button,
-    Buttons,
-    Foot,
-    GitLink,
-    Philosophy
-} from '../GlobalStyles'
-import {
     Wrapper,
-    Author,
-    AuthorName,
-    AuthorImg,
-    AuthorDetail,
-    Project,
-    UserPro,
-    Names,
-    Details,
-    Supported,
-    NameSup,
-    DetailsSup,
-    UserSup
 } from './UsersStyles'
 
 const Users = () => {
     return (
-            <>
+        <>
             <Wrapper>
-                <TopNav>
-                    <Logo>Ciues-Auxilium</Logo>
-                    <Buttons>
-                        <Button to='/login' >Log In</Button>
-                        <Button to='/account' >Account</Button>
-                        </Buttons>
-                    </TopNav>
-                <Author>
-                    <AuthorName>AUTHOR NAME && BIO</AuthorName>
-                    <AuthorImg>IMG</AuthorImg>
-                    <AuthorDetail>Detail</AuthorDetail>
-                </Author>
-                <Project>
-                    <UserPro>USER’S PROJECTS</UserPro>
-                    <Names>Name of PROJECTS</Names>
-                    <Details>Detail of PROJECTS</Details>
-                </Project>
-                <Supported>
-                    <UserSup>USER’S SUPPORTED PROJECTS</UserSup>
-                    <NameSup>Name of SUPPORTED</NameSup>
-                    <DetailsSup>Detail of SUPPORTED</DetailsSup>
-                </Supported>
-                <Foot>
-                    <GitLink>GitHub</GitLink>
-                    <Philosophy>Philosophy</Philosophy>
-                </Foot>
+                <TopNavBar />
+                <AuthorView />
+                <ProjectView />
+                <SupportedView />
+                <Footer />
             </Wrapper>
         </>
     )
-};
+}
 
-export default Users;
+export default Users
