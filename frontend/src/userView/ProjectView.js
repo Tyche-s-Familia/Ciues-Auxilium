15ecc53de4097b607069bbe9e7bd149411da8a97
Nevsimao <coday.ink@gmail.com>
2021-04-26 00:30:30 -0400

    moved to it's own comp

---
+++ frontend/src/userView/ProjectView.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import {
+    Project,
+    UserPro,
+    Names,
+    Details
+} from './UsersStyles'
+
+const ProjectView = () => {
+    return (
+        <Project>
+            <UserPro>USER’S PROJECTS</UserPro>
+            <Names>Name of PROJECTS</Names>
+            <Details>Detail of PROJECTS</Details>
+        </Project>
+    )
+}
+
+export default ProjectView
