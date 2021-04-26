3e5194336175f5852ca8a45dd158e947d9d3c943
Nevsimao <coday.ink@gmail.com>
2021-04-26 00:23:29 -0400

    moved to a separate comp

--- frontend/src/feedView/Feed.js
+++ frontend/src/feedView/Feed.js
@@ -1,82 +1,26 @@
-import React from "react";
+import React from "react"
+
 import SearchBar from './SearchBar'
+import TopNavBar from './TopNavBar'
+import NavBar from './NavBar'
+import MainArea from './MainArea'
+import Footer from './Footer'
 
 import {
   Wrapper,
-  TopNav,
-  Nav,
-  NavItems,
-  NavItem,
-  Main,
-  TopMain,
-  MidMain,
-  MidBox1,
-  MidBox2,
-  MidBox3,
-  MidBox4,
-  Buttons,
-  Logo,
-  Button,
-  GitLink,
-  Philosophy,
-  Img,
-  Description,
-  Info,
-  Foot
-} from '../GlobalStyles';
+} from '../GlobalStyles'
 
-export default function Feed() {
+const Feed = () => {
 
     return (
         <Wrapper>
-          <TopNav>
-              <Logo>Ciues-Auxilium</Logo>
-            <Buttons>
-              <Button to='/login' >Log In</Button>
-              <Button to='/account' >Account</Button>
-            </Buttons>
-          </TopNav>
+          <TopNavBar />
           <SearchBar />
-          <Nav>
-              <NavItems>
-                <NavItem to="/development">Development</NavItem>
-                <NavItem to="/arts">Arts</NavItem>
-                <NavItem to="/journalism">Journalism</NavItem>
-                <NavItem to="/entertainment">Entertainment</NavItem>
-              </NavItems>
-          </Nav>
-          <Main>
-            <TopMain>
-              <Button to='projects' >PROJECTS</Button>
-              <Button to='updates' >UPDATES</Button>
-            </TopMain>
-            <MidMain>
-              <MidBox1>
-                <Img>Img</Img>
-                <Info>Info</Info>
-                <Description>Description</Description>
-              </MidBox1>
-              <MidBox2>
-                <Img>Img</Img>
-                <Info>Info</Info>
-                <Description>Description</Description>
-              </MidBox2>
-              <MidBox3>
-                <Img>Img</Img>
-                <Info>Info</Info>
-                <Description>Description</Description>
-              </MidBox3>
-              <MidBox4>
-                <Img>Img</Img>
-                <Info>Info</Info>
-                <Description>Description</Description>
-              </MidBox4>
-            </MidMain>
-          </Main>
-          <Foot>
-            <GitLink to='github' >GitHub</GitLink>
-            <Philosophy to='philosophy' >Philosophy</Philosophy>
-          </Foot>
+          <NavBar />
+          <MainArea />
+          <Footer />
         </Wrapper>
-    );
-};
+    )
+}
+
+export default Feed
