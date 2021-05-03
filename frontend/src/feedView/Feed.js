fd9ebd0d8ebfe50abe814a668a6df2d46b8165d3
Nevsimao <coday.ink@gmail.com>
2021-05-03 11:07:39 -0400

    making connections

--- frontend/src/feedView/Feed.js
+++ frontend/src/feedView/Feed.js
@@ -6,9 +6,18 @@ import NavBar from './NavBar'
 import MainArea from './MainArea'
 import Footer from './Footer'
 
+import { atom, useRecoilState } from 'recoil'
+
 import {
   Wrapper,
 } from '../GlobalStyles'
+
+const axios = require('axios').default
+
+export const searchState = atom({
+	key: 'searchState',
+	defalut: '',
+})
 
 const Feed = () => {
     return (
