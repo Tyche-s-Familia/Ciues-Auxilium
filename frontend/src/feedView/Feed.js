c41b82092cac36e1172b73abb3a95672a70f6cb5
Nevsimao <coday.ink@gmail.com>
2021-05-03 11:55:10 -0400

    trying to display data

--- frontend/src/feedView/Feed.js
+++ frontend/src/feedView/Feed.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import SearchBar from './SearchBar'
 import TopNavBar from './TopNavBar'
 import NavBar from './NavBar'
 import MainArea from './MainArea'
 import Footer from './Footer'
+
+import APIurl from "../config"
 
 import { atom, useRecoilState } from 'recoil'
 
@@ -20,15 +22,44 @@ export const searchState = atom({
 })
 
 const Feed = () => {
+
+	const [data, setData] = useState()
+	const [search, setSearch] = useRecoilState(searchState)
+
+  useEffect(() => {
+    if (search !== null && search !== undefined && search !== '') {
+      axios
+      .get(`${APIurl}projects`, {mode:'cors'})
+      .then((res) => setData(
+        res.data.filter((item) => item.title.lowerCase().includes(search.toLowerCase()))
+      )
+      )
+      .catch(console.error)
+    }
+    else {
+      axios
+      .get(`${APIurl}projects`, {mode:'cors'})
+      .then((res) => {
+        setData(res.data)
+      })
+      .catch(console.error)
+    }
+  }, [search])
+
+  if (data) {
     return (
-        <Wrapper>
-          <TopNavBar />
-          <SearchBar />
-          <NavBar />
-          <MainArea />
-          <Footer />
-        </Wrapper>
+      <Wrapper>
+        <TopNavBar />
+        <SearchBar />
+        <NavBar />
+        <MainArea data={data} />
+        <Footer />
+      </Wrapper>
     )
+  }
+  else {
+    return <h1>Loading</h1>
+  }
 }
 
 export default Feed
