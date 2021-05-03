7705a1eb162f84bc5bba5369bc74068335ee9285
Nevsimao <coday.ink@gmail.com>
2021-05-03 11:07:17 -0400

    making connections

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import APIurl from "./config"
+import axios from "axios"
 import { BrowserRouter as Router, Route, Switch, useHistory } from "react-router-dom";
 
 import Feed from './feedView/Feed'
@@ -9,28 +11,18 @@ import {
   GlobalStyles
 } from './GlobalStyles'
 
-// import axios from 'axios';
-import useSWR from 'swr'
-
-// import APIurl from './config'
-
 export default function App() {
 
-  const { data, error } = useSWR(`${APIurl}/users/list`)
-
-  // if (error) return <div>failed to load</div>
-  // if (!data) return <div>loading...</div>
-  // const history = useHistory();
-  // console.log(data.username)
-  // axios
-	// 		.get(`${APIurl}/projects/list`, {mode:'cors'})
-	// 		.then(function (response) {
-	// 			console.log(response);
-	// 		})
-	// 		// .then(() => history.push('/account'))
-	// 		.catch(function (error) {
-	// 			console.log(error);
-	// 		});
+  const history = useHistory();
+  axios
+			.get(`${APIurl}users`, {mode:'cors'})
+			.then(function (response) {
+				console.log(response);
+			})
+			.then(() => history.push('/account'))
+			.catch(function (error) {
+				console.log(error);
+			});
 
   return (
     <Router>
