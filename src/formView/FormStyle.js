c763135c13001c671796d8057221b0d2af7609b8
Nevsimao <coday.ink@gmail.com>
2021-05-04 11:42:03 -0400

    styles for login and signup

---
+++ src/formView/FormStyle.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+
+export const SignInNav = styled.nav`
+	margin: 0 auto;
+	padding: 16px 24px;
+	height: 72px;
+	font-size: 3rem;
+	.nav-tag {
+		color: #222f65;
+		font-weight: 700;
+		text-decoration: none;
+	}
+`;
+
+export const HeaderOne = styled.h1`
+	font-size: 3rem;
+`;
+
+export const SignUpContainer = styled.div`
+	margin: 0 auto;
+	text-align: center;
+`;
+
+export const PrimaryButton = styled.button`
+	width: 350px;
+	height: 48px;
+	background-color: #111b47;
+	color: #fff;
+	font-size: 1.2rem;
+	border-radius: 2px;
+	margin: 0 auto;
+	font-family: 'Inconsolata', monospace;
+	margin: 24px 0;
+`;
+
+export const Label = styled.label`
+	color: #767676;
+	margin: 0 auto;
+	text-align: left;
+	display: flex;
+	width: 344px;
+`;
+
+export const InputStyle = styled.input`
+	width: 344px;
+	height: 48px;
+	border: none;
+	border-bottom: 1px solid #b0b8bc;
+	font-family: 'Inconsolata', monospace;
+	font-size: 1rem;
+	margin-bottom: 24px;
+`;
